b19b7c17007819f610d50c47e1cbca7ed71e2d91
gabrielaneves1 <gabriela.neves@ucsal.edu.br>
2025-08-19 23:31:32 -0300

    Add files via upload

---
+++ 2025-2pma1-gabrielaneves1-main/src/main/java/cms/user/GuestUser.java
@@ -0,0 +1,12 @@
+package cms.user;
+
+public class GuestUser extends User {
+    public GuestUser(int id, String nome, String email, String password) {
+        super(id, nome, email, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "Guest";
+    }
+}
